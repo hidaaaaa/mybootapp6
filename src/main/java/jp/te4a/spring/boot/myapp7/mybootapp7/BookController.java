f46824a6efe82f60bc3421ac03a1a7353ee95d90
hidaaaa <te2a220637@gmail.com>
2025-05-27 12:27:46 +0900

    aaaaaaaaaaa

--- src/main/java/jp/te4a/spring/boot/myapp7/mybootapp7/BookController.java
+++ src/main/java/jp/te4a/spring/boot/myapp7/mybootapp7/BookController.java
@@ -1,5 +1,5 @@
 package jp.te4a.spring.boot.myapp7.mybootapp7;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,18 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
+ 
 @Controller
 public class BookController {
     @Autowired
     BookService bookService;
-
+ 
     @RequestMapping("books/list")
     public String index(Model model) {
         model.addAttribute("msg", "this is setting message");
         return "books/list";
     }
-
+ 
     @RequestMapping(value = "books/list", method = RequestMethod.POST)
     public ModelAndView postForm(@RequestParam("id") String id,
             @RequestParam("title") String title, @RequestParam("writter") String writter,
