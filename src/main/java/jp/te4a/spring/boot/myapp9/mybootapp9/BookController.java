7cb040be0bef9669da9e4d7ce2578b68a606e52c
hidaaaa <te2a220637@gmail.com>
2025-06-03 12:04:21 +0900

    kadai9

--- src/main/java/jp/te4a/spring/boot/myapp9/mybootapp9/BookController.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/mybootapp9/BookController.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp8.mybootapp8;
+package jp.te4a.spring.boot.myapp9.mybootapp9;
  
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +26,7 @@ public class BookController {
  }
  @PostMapping(path="create")
  String create(BookForm form, Model model) {
- bookService.create(form);
+ bookService.save(form);
  return "redirect:/books";
  }
  @PostMapping(path = "edit", params = "form")
