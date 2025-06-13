33a7854d8809cfa498a9c0ec4faa27a609547d57
hidaaaa <te2a220637@gmail.com>
2025-06-13 14:18:32 +0900

    kadai11

--- src/main/java/jp/te4a/spring/boot/myapp11/mybootapp11/BookController.java
+++ src/main/java/jp/te4a/spring/boot/myapp11/mybootapp11/BookController.java
@@ -1,9 +1,11 @@
-package jp.te4a.spring.boot.myapp10.mybootapp10;
+package jp.te4a.spring.boot.myapp11.mybootapp11;
  
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,9 +27,12 @@ public class BookController {
  return "books/list";
  }
  @PostMapping(path="create")
- String create(BookForm form, Model model) {
- bookService.save(form);
- return "redirect:/books";
+ String create(@Validated BookForm form,BindingResult result, Model model) {
+    if(result.hasErrors()){
+        return list(model);
+    }
+    bookService.save(form);
+    return "redirect:/books";
  }
  @PostMapping(path = "edit", params = "form")
  String editForm(@RequestParam Integer id, BookForm form) {
@@ -36,7 +41,10 @@ public class BookController {
  return "books/edit";
  }
 @PostMapping(path = "edit")
- String edit(@RequestParam Integer id, BookForm form) {
+ String edit(@RequestParam Integer id,@Validated BookForm form,BindingResult result) {
+    if(result.hasErrors()){
+        return editForm(id,form);
+    }
  bookService.update(form);
  return "redirect:/books";
  }
